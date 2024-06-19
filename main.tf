41421aed9a0d3cecb084786e5f649ac08926b79b
sravya0301 <168750563+sravya0301@users.noreply.github.com>
2024-06-19 17:38:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1c"] 
+    availability_zones    = ["us-east-1f", "us-east-1c"] 
     
   }
 
