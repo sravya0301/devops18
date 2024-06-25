d224392c5d64db3e0bab5f23a43bb02b3cc539b9
sravya0301 <168750563+sravya0301@users.noreply.github.com>
2024-06-25 19:12:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-08a0d1e16fc3f61ea"
+    image_id           = "ami-0eaf7c3456e7b5b68"
     instance_type = "t2.micro"
     key_name = "kpnpem"
 }
