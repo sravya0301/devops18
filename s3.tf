84c88bdf1bdc5b2748d051d886537e4aab06f307
sravya0301 <168750563+sravya0301@users.noreply.github.com>
2024-06-25 17:04:37 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "sravya.devops.project.bucket"
+  bucket = "sravya.devops.project.bucket1"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -25,7 +25,7 @@ status = "Enabled"
 
 terraform {
     backend "s3" {
-        bucket = "sravya.devops.project.bucket"
+        bucket = "sravya.devops.project.bucket1"
         key = "prod/terraform.tfstate"
         region = "us-east-1"
     }
